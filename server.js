fb9933277b4cbd352350c9df12508036f5909484
moheeboy <30660646+moheeboy@users.noreply.github.com>
2017-08-20 10:58:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/articleOne', function (req, res){
+  res.sendFile(path.join(__dirname, 'ui', 'articleOne.html'));
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
@@ -17,9 +21,6 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
-app.get('/articleOne', function (req, res){
-  res.sendFile(path.join(__dirname, 'ui', 'articleOne.html'));
-});
 
 app.get('/articleTwo', function (req, res){
   res.send('Article two is requested and will be sered.');  
