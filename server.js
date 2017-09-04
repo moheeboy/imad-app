5ae80c98ffe18fc3ad1732b8fa8ed49c3de96fc7
moheeboy <30660646+moheeboy@users.noreply.github.com>
2017-09-04 12:17:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -136,14 +136,26 @@ app.get('/createUser', function(req,res){
         if(err){
             res.status(500).send(err.toString());
        }else{
-           if(result.rows.length === 0 ){
-               res.status(404).send('Article not found!');
-           }else{
-               var articleData = result.rows[0];
-               res.send("User Sucessfully Created");
-           }
+           res.send("User Sucessfully Created");
        }
     });
+});
+
+app.get('/verifyUser', function(req, res){
+    
+      pool.query("SELECT * dbuser where username = $1", [username], function(err, result){
+          if(err){
+            res.status(500).send(err.toString());
+       }else{
+           if (res.rows.length === 0 ){
+              res.send("No such user found"); 
+           }else{
+               res.send("User found sucessfully");
+           }
+           
+       }
+      });
+      
 });
 
 app.get('/ui/style.css', function (req, res) {
